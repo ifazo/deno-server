f0054776414250571fe8bb951023e841f1c39ca3
Ifazo <ziaulkarimifaz@outlook.com>
2025-02-18 01:12:41 +0600

    deno.exit remove

--- src/db.ts
+++ src/db.ts
@@ -6,11 +6,6 @@ await load({ export: true });
 const MONGODB_URI = Deno.env.get("MONGODB_URI");
 const DB_NAME = Deno.env.get("DB_NAME");
 
-if (!MONGODB_URI) {
-  console.error("MONGODB_URI is not set");
-  Deno.exit(1);
-}
-
 const client = new MongoClient(MONGODB_URI);
 
 try {
@@ -18,7 +13,6 @@ try {
   console.log("Connected to MongoDB");
 } catch (error) {
   console.error("Error connecting to MongoDB:", error);
-  Deno.exit(1);
 }
 
 const db = client.db(DB_NAME);
